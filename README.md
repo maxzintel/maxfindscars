421eb2c70d9d8e2aee32cd46f2da1c90a1b18845
maxzintel <maxzintel@gmail.com>
2023-05-02 22:11:51 -0400

    Add contact button to footer.

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
 # MaxFindsCars
 
-## New Full Run Directions
+## Local Dev Environment Setup Directions
 
 * `cd client`
 * `npm install`
 * `npm run build:css` builds the tailwind css file and watches for changes.
 * Open a new terminal window, navigate to this code repo.
-* `cd client`
+* `cd ../server`
+* `npm run dev:backend` starts the backend server on port 5000.
+* `cd ../client`
 * `npm run build` builds the frontend files that React serves to the browser.
 * `npm run start` starts the frontend on port 3000
-* Open a new terminal window, navigate to this code repo.
-* `npm run dev` starts the backend server on port 5000.
-
-## Old Directions
-
-* npm install
-* npx tailwindcss -i ./public/css/input.css -o ./public/style.css --watch
-* npm run dev
+* Navigate to the frontend at `localhost:3000`, which should be proxying requests to the backend at `localhost:5000`.
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,23 @@
 // Footer.js
-import React from 'react';
+import React, {useState} from 'react';
+import ContactInfo from './ContactInfo';
 
 const Footer = () => {
+  const [showContactInfo, setShowContactInfo] = useState(false);
+
+  const handleContactClick = () => {
+    setShowContactInfo(true);
+  };
+
+  const handleCloseInfo = () => {
+    setShowContactInfo(false);
+  };
+
   return (
     <footer className="footer-container m-3 px-10 py-4 flex items-center justify-center bg-antiquewhite">
       <p className="mr-auto">2023, MaxFindsCars LLC</p>
+      {showContactInfo && <ContactInfo onClose={handleCloseInfo} />}
+      <button onClick={handleContactClick} className="mx-2 p-1 outline-2 border-black border-2 bg-yellow font-bold">CONTACT</button>
       <a href="https://twitter.com/maxjzin" className="mx-1">
         <img src={`${process.env.PUBLIC_URL}/logos/Twitter-logo.svg.png`} alt="Twitter Icon" className="h-6" />
       </a>
