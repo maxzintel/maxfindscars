865e7eb87d63114694e1ce049d687fb21e7ee0f3
Max Zintel <43785951+maxzintel@users.noreply.github.com>
2023-05-16 09:37:16 -0400

    Update GoogleAnalytics.js

--- client/src/components/GoogleAnalytics.js
+++ client/src/components/GoogleAnalytics.js
@@ -14,6 +14,15 @@ const GoogleAnalytics = () => {
           gtag('config', 'G-5BPLJYLL4C');
         `}
       </script>
+      <script>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-522R2RD');
+        `}
+      </script>
     </Helmet>
   );
 };
